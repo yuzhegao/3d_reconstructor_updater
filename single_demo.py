ba67b12c67b3d5e88f7d9fded78fdc927429deac
yuzhegao <yuzhegao0721@gmail.com>
2018-02-27 21:17:05 -0800

    update single_demo.py:a successful version

--- single_demo.py
+++ single_demo.py
@@ -3,19 +3,39 @@ from __future__ import print_function,division
 import os.path
 
 import time
+import argparse
 from data_prepare.bulid_data import singleDataset,single_collate
 from layer.voxel_net2 import singleNet
 from layer.voxel_func import *
 from utils.utils_rw import *
 
+parser = argparse.ArgumentParser(description='single-view CNN demo')
+parser.add_argument('--data', metavar='DIR',default='./dataset/CubeData',
+                    help='path to dataset')
+
+parser.add_argument('--data-name', default='csg', type=str, metavar='PATH',
+                    help='name of dataset (default: csg)')
+## this arg: name of log, data list file(.txt)
+
+parser.add_argument('--gpu', default=0, type=int, metavar='N',
+                    help='the index  of GPU where program run')
+
+parser.add_argument('-bs',  '--batch-size', default=1, type=int,
+                    metavar='N', help='mini-batch size (default: 1)')
+
+parser.add_argument('--resume', default='latest_model_multi.pth', type=str, metavar='PATH',
+                    help='name of latest checkpoint file (default: latest_model_multi.pth)')
+args=parser.parse_args()
+
 
 def IOU(model1,model2):
     insect= np.sum(model1[model2])
     union= np.sum(model1)+np.sum(model2)-insect
+    print ("insect:{} union:{}".format(insect,union))
     return insect*1.0/union
 
 def eval_model(model_name):
-    with open('./dataset/CsgData/trainval/binvox/'+model_name+'.binvox', 'rb') as f:
+    with open(args.data+'/binvox/'+model_name+'.binvox', 'rb') as f:
         # m1 = read_as_coord_array(f)
         m1 = read_as_3d_array(f)
 
@@ -29,26 +49,33 @@ def eval_model(model_name):
 
 
 np.set_printoptions(threshold='nan')
-resume='./model_epoch/csg_single_model.pth'
+resume='./model/'+args.resume
 
 model=singleNet()
+##model.eval()  this is the problem!!!
 
 is_GPU=torch.cuda.is_available()
 if is_GPU:
-    torch.cuda.set_device(0)
+    torch.cuda.set_device(args.gpu)
 
 if os.path.exists(resume):
+    print('load the checkpoint file {}'.format(resume))
     if is_GPU:
         model.load_state_dict(torch.load(resume)['model'])
     else:
         model.load_state_dict(torch.load(resume,map_location=lambda storage, loc: storage)['model'])
 
     print ('load trained model success')
+else:
+    print ("Warning! no resume file to load\n")
 
-data_rootpath='./dataset/CsgData'
-dataset=singleDataset(data_rootpath)
+data_rootpath=args.data
+dataset=singleDataset(data_rootpath,data_name=args.data_name,test=True)
 
-for i in xrange(210,213):
+
+## very strange:  for i in xrange(10010,10013): use cube_single_train.pth trained in server,get a very high iou(0.8)
+## but in evaluate_single, the iou is low(0.4)
+for i in xrange(20,23):
     img_id, test_img = dataset.pull_img(i)
     # test_img = np.array(test_img).astype(np.float32)
     # cv2.imshow('mat',test_img)
@@ -66,9 +93,9 @@ for i in xrange(210,213):
     end = time.time()
 
     result = up.data[0, :, :, :].numpy()
-    print (result)
+    #print (result)
     print ("each forward use:{}s".format(end - init))
-    result = result >= 0.5
+    result = (result >= 0.5)
 
     with open('0_0.binvox', 'rb') as f:
         # m1 = read_as_coord_array(f)
